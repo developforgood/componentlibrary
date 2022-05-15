80d4a7a50c38be450f29691cf32552bb4feb4b9c
anguy212 <32521966+anguy212@users.noreply.github.com>
2022-05-14 23:08:51 -0700

    added inputs documentation

--- src/stories/BasicInput.stories.jsx
+++ src/stories/BasicInput.stories.jsx
@@ -5,16 +5,40 @@ export default {
     title: 'Components/FormField/BasicInput',
     component: BasicInput,
     argTypes: {
-      //ActiveColor: {control: 'color'},
-      name: {control: 'text'},
-      LabelText: {control: 'text'},
-      PlaceHolder: {control: 'text'},
-      Required: {control: 'boolean'},
-      ErrorMessage: {control: 'text'},
-      Type: {control: 'text'},
-      Pattern: {control: 'text'},
-      value: {control: 'text'},
-      OnChange: {control: 'function'}
+      name: {
+        control: 'text',
+        description: "Name of corresponding value in useState value array. Will place what the user types into this value in the array."
+      },
+      LabelText: {
+        control: 'text',
+        description: "Name of the label that sits on top of the input."
+      },
+      PlaceHolder: {
+        control: 'text',
+        description: "Placeholder for the input when the user has not typed anything in yet. May be kept blank if needed"
+      },
+      Required: {
+        control: 'boolean',
+        description: "Decides if input type is required to complete form. If true, can not proceed until info is entered"
+      },
+      ErrorMessage: {
+        control: 'text',
+        description: "Error message that pops up when info entered is incorrect"
+      },
+      Type: {
+        control: 'text',
+        description: "Type of input. ex/ date, time, text"
+      },
+      Pattern: {
+        control: 'text',
+        description: "Regex pattern to check input with. Will use this to determine if error message should show"
+      },
+      OnChange: {
+        control: 'function',
+        description: `What -> a function that should dictate what do when value of input changes. 
+        Recommended -> const onChange = (e) => { setValues({ ...values, [e.target.name]: e.target.value });}; 
+        Sample -> see example of how to use component here https://github.com/anguy212/DFG_inputs_ailceTest`
+      }
     }
   };
 
--- src/stories/DropDownInput.stories.jsx
+++ src/stories/DropDownInput.stories.jsx
@@ -5,13 +5,28 @@ export default {
     title: 'Components/FormField/DropDownInput',
     component: DropDownInput,
     argTypes: {
-      //ActiveColor: {control: 'color'},
-      name: {control: 'text'},
-      value: {control: 'text'},
-      LabelText: {control: 'text'},
-      PlaceHolder: {control: 'text'},
-      Options: {options: ['item a', 'item b', 'item c']},
-      OnChange: {control: 'function'}
+      name: {
+        control: 'text',
+        description: "Name of corresponding value in useState value array. Will place what the user types into this value in the array."
+      },
+      LabelText: {
+        control: 'text',
+        description: "Name of the label that sits on top of the input."
+      },
+      PlaceHolder: {
+        control: 'text',
+        description: "Placeholder for the input when the user has not typed anything in yet. May be kept blank if needed"
+      },
+      Options: {
+        options: ['item a', 'item b', 'item c'],
+        description: "Options for what choices are in the selection in array format"
+      },
+      OnChange: {
+        control: 'function',
+        description: `What -> a function that should dictate what do when value of input changes. 
+        Recommended -> const onDropChange = (n, value) => {setDropDownValues({...dropDownValues, [n]: value});} 
+        Sample -> see example of how to use component here https://github.com/anguy212/DFG_inputs_ailceTest`
+      }
     }
   };
 
